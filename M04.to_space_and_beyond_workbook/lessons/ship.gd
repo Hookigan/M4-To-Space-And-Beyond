0dfa25c453993fe28baa833ac04bf88c02ee8020
Hookigan <jafiglietti@s.sfusd.edu>
2025-11-06 11:24:48 -0800

    M4L6

--- M04.to_space_and_beyond_workbook/lessons/ship.gd
+++ M04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -1,16 +1,16 @@
 extends Sprite2D
 
-var normal_speed := 600
-var boost_speed := 1500.0
+var normal_speed := 1000
+var boost_speed := 2000.0
 
 var max_speed := normal_speed
 var velocity := Vector2(0,0)
-var steering_factor := 1.0
+var steering_factor := 10.0
 
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0,0)
-	direction.x = Input.get_axis("move_left", "Move_right")
+	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 	
 	if direction.length() > 1.0:
